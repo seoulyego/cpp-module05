89fe0481709432f3718429b988504d91186ae59c
yeongo <yeongo@student.42seoul.kr>
2023-10-22 16:58:40 +0900

    ex02 main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,15 +9,25 @@ void	i_wanna_go_home(void) {
 
 int	main(void) {
 	Bureaucrat bru("foo", 1);
-    ShrubberyCreationForm test0("srurup");
-    PresidentialPardonForm test1("president");
-    RobotomyRequestForm test2("robot");
-    std::cout << test0.getSignGrade() << std::endl;
-    test0.beSigned(bru);
-    test1.beSigned(bru);
-    test2.beSigned(bru);
-    bru.executeForm(test0);
-    bru.executeForm(test1);
-    bru.executeForm(test2);
+	std::cout << bru << std::endl;
+	std::cout << std::endl;
+	ShrubberyCreationForm test0("syururup");
+	PresidentialPardonForm test1("president");
+	RobotomyRequestForm test2("robot");
+	std::cout << test0 << std::endl;
+	std::cout << test1 << std::endl;
+	std::cout << test2 << std::endl;
+	std::cout << std::endl;
+	test0.beSigned(bru);
+	test1.beSigned(bru);
+	test2.beSigned(bru);
+	std::cout << std::endl;
+	std::cout << test0 << std::endl;
+	std::cout << test1 << std::endl;
+	std::cout << test2 << std::endl;
+	std::cout << std::endl;
+	bru.executeForm(test0);
+	bru.executeForm(test1);
+	bru.executeForm(test2);
 	return 0;
 }
